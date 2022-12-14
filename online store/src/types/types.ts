297b6d188f01dfadb09060171526e8155bdfa9fb
AlexeiTim <timashkov060896@gmail.com>
2022-12-14 20:33:32 +0300

    feat:add data and type

---
+++ online store/src/types/types.ts
@@ -0,0 +1,33 @@
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: Brand;
+  category: Category;
+  thumbnail: string;
+  images: string[];
+}
+
+type Brand =
+  | "Philips"
+  | "Samsung"
+  | "Xiaomi"
+  | "Bosch"
+  | "LG"
+  | "ATLANT"
+  | "BEKO"
+  | "Horizont"
+  | "Media"
+  | "Atlanta"
+  | "DeLonghi";
+type Category =
+  | "Vacuum cleaners"
+  | "Сoffee machines"
+  | "Fridges"
+  | "Microwaves"
+  | "Kettles"
+  | "Washing machine";
